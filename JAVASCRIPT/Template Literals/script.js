a444caee3ec510827663f5d299c79917d2a72602
Mehmet Enes Erdogan <77643160+menerglobal@users.noreply.github.com>
2021-04-13 17:25:22 +0200

    Add files via upload

---
+++ JAVASCRIPT/Template Literals/script.js
@@ -0,0 +1,18 @@
+// Template  Literals
+
+const fullName = 'Enes Erdogan';
+const city = 'Izmir';
+const yearOfBirth = 1997;
+
+let val;
+
+
+val = 'my name is '+fullName+
+      ' I\'m '+(2021-yearOfBirth)+' years old '+
+      ' and I live in '+city;
+
+val = `my name is ${fullName} I'm ${2021-yearOfBirth} years old and I live in ${city}`;
+val = `my name is ${fullName} I'm ${(2021-yearOfBirth>=18)?'over 18':'under 18'} and I live in ${city}`;
+
+console.log(val);
+
