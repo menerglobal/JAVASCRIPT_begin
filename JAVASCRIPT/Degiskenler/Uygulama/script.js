f03c1208eb4406a8e0464a23a245cfe108378f82
Mehmet Enes Erdogan <77643160+menerglobal@users.noreply.github.com>
2022-02-10 11:14:16 +0100

    Update script.js

--- JAVASCRIPT/Degiskenler/Uygulama/script.js
+++ JAVASCRIPT/Degiskenler/Uygulama/script.js
@@ -54,7 +54,7 @@ console.log(totalOrder3);
 
 // ** Aşağıda verilen doğum yılına göre yaş hesaplayınız.
 
-const yearOfBirth = 1997;
+const yearOfBirth = 1996;
 // yearOfBirth = 1997;
 
 console.log(new Date().getFullYear()-yearOfBirth);
