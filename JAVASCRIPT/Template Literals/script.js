03afe77932204af61128bcb230b7dc4310f378ce
Mehmet Enes Erdogan <77643160+menerglobal@users.noreply.github.com>
2022-02-12 14:44:00 +0100

    Update script.js

--- JAVASCRIPT/Template Literals/script.js
+++ JAVASCRIPT/Template Literals/script.js
@@ -8,11 +8,11 @@ let val;
 
 
 val = 'my name is '+fullName+
-      ' I\'m '+(2021-yearOfBirth)+' years old '+
+      ' I\'m '+(2022-yearOfBirth)+' years old '+
       ' and I live in '+city;
 
-val = `my name is ${fullName} I'm ${2021-yearOfBirth} years old and I live in ${city}`;
-val = `my name is ${fullName} I'm ${(2021-yearOfBirth>=18)?'over 18':'under 18'} and I live in ${city}`;
+val = `my name is ${fullName} I'm ${2022-yearOfBirth} years old and I live in ${city}`;
+val = `my name is ${fullName} I'm ${(2022-yearOfBirth>=18)?'over 18':'under 18'} and I live in ${city}`;
 
 console.log(val);
 
